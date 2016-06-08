bba22164408dea4b38c5fea98563da86188a8941
tankai <kai.tank@imike.com>
2016-06-08 16:06:30 +0800

    curd 点赞和评价

--- basic-biz/src/main/java/com/duantuke/basic/service/impl/DuantukeCommentServiceImpl.java
+++ basic-biz/src/main/java/com/duantuke/basic/service/impl/DuantukeCommentServiceImpl.java
@@ -2,41 +2,63 @@ package com.duantuke.basic.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.duantuke.basic.face.service.DuantukeCommentService;
+import com.duantuke.basic.mappers.DuantukeCommentMapper;
 import com.duantuke.basic.po.DuantukeComment;
+import com.duantuke.basic.po.DuantukeCommentExample;
 
-
+@Service
 public class DuantukeCommentServiceImpl implements DuantukeCommentService{
 
+	@Autowired
+	private DuantukeCommentMapper duantukeCommentMapper;
+	
+	
+	/**
+	 * 根据类型计算评论数
+	 */
 	@Override
 	public int countDuantukeComment(DuantukeComment duantukeComment) {
-		// TODO Auto-generated method stub
-		return 0;
+		DuantukeCommentExample example = new DuantukeCommentExample();
+		DuantukeCommentExample.Criteria hoCriteria = example.createCriteria();
+		hoCriteria.andBusinessTypeEqualTo(duantukeComment.getBusinessType());
+		hoCriteria.andFidEqualTo(duantukeComment.getFid());
+		return duantukeCommentMapper.countByExample(example);
 	}
 
 	@Override
 	public int deleteDuantukeComment(DuantukeComment duantukeComment) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		DuantukeCommentExample example = new DuantukeCommentExample();
+		DuantukeCommentExample.Criteria hoCriteria = example.createCriteria();
+		hoCriteria.andBusinessTypeEqualTo(duantukeComment.getBusinessType());
+		hoCriteria.andFidEqualTo(duantukeComment.getFid());
+		return duantukeCommentMapper.deleteByExample(example);
 	}
 
 	@Override
 	public int deleteByPrimaryKey(Long duantukeCommentId) {
-		// TODO Auto-generated method stub
-		return 0;
+		return duantukeCommentMapper.deleteByPrimaryKey(duantukeCommentId);
 	}
 
 	@Override
 	public int insert(DuantukeComment duantukeComment) {
-		// TODO Auto-generated method stub
-		return 0;
+		return duantukeCommentMapper.insertSelective(duantukeComment);
 	}
 
 	@Override
 	public List<DuantukeComment> selectByDuantukeComment(
 			DuantukeComment duantukeComment) {
-		// TODO Auto-generated method stub
-		return null;
+
+		DuantukeCommentExample example = new DuantukeCommentExample();
+		DuantukeCommentExample.Criteria hoCriteria = example.createCriteria();
+		hoCriteria.andBusinessTypeEqualTo(duantukeComment.getBusinessType());
+		hoCriteria.andFidEqualTo(duantukeComment.getFid());
+	
+		return duantukeCommentMapper.selectByExample(example);
 	}
 	
 }
--- basic-biz/src/main/java/com/duantuke/basic/service/impl/DuantukeLikeServiceImpl.java
+++ basic-biz/src/main/java/com/duantuke/basic/service/impl/DuantukeLikeServiceImpl.java
@@ -2,38 +2,67 @@ package com.duantuke.basic.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.duantuke.basic.face.service.DuantukeLikeService;
+import com.duantuke.basic.mappers.DuantukeLikeMapper;
 import com.duantuke.basic.po.DuantukeLike;
+import com.duantuke.basic.po.DuantukeLikeExample;
 
-
+@Service
 public class DuantukeLikeServiceImpl implements DuantukeLikeService{
 
+
+	@Autowired
+	private DuantukeLikeMapper duantukeLikeMapper;
+	
+	
+	/**
+	 * 根据类型计算评论数
+	 */
 	@Override
 	public int countDuantukeLike(DuantukeLike duantukeLike) {
-		// TODO Auto-generated method stub
-		return 0;
+		DuantukeLikeExample example = new DuantukeLikeExample();
+		DuantukeLikeExample.Criteria hoCriteria = example.createCriteria();
+		hoCriteria.andBusinessTypeEqualTo(duantukeLike.getBusinessType());
+		hoCriteria.andFidEqualTo(duantukeLike.getFid());
+		hoCriteria.andCustomerIdEqualTo(duantukeLike.getCustomerId());
+		return duantukeLikeMapper.countByExample(example);
 	}
 
 	@Override
 	public int deleteDuantukeLike(DuantukeLike duantukeLike) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		DuantukeLikeExample example = new DuantukeLikeExample();
+		DuantukeLikeExample.Criteria hoCriteria = example.createCriteria();
+		hoCriteria.andBusinessTypeEqualTo(duantukeLike.getBusinessType());
+		hoCriteria.andFidEqualTo(duantukeLike.getFid());
+		hoCriteria.andCustomerIdEqualTo(duantukeLike.getCustomerId());
+		return duantukeLikeMapper.deleteByExample(example);
 	}
 
 	@Override
 	public int deleteByPrimaryKey(Long duantukeLikeId) {
-		// TODO Auto-generated method stub
-		return 0;
+		return duantukeLikeMapper.deleteByPrimaryKey(duantukeLikeId);
 	}
 
 	@Override
 	public int insert(DuantukeLike duantukeLike) {
-		// TODO Auto-generated method stub
-		return 0;
+		return duantukeLikeMapper.insertSelective(duantukeLike);
 	}
 
 	@Override
-	public List<DuantukeLike> selectByDuantukeLike(DuantukeLike duantukeLike) {
-		// TODO Auto-generated method stub
-		return null;
-	}}
+	public List<DuantukeLike> selectByDuantukeLike(
+			DuantukeLike duantukeLike) {
+
+		DuantukeLikeExample example = new DuantukeLikeExample();
+		DuantukeLikeExample.Criteria hoCriteria = example.createCriteria();
+		hoCriteria.andBusinessTypeEqualTo(duantukeLike.getBusinessType());
+		hoCriteria.andFidEqualTo(duantukeLike.getFid());
+		hoCriteria.andCustomerIdEqualTo(duantukeLike.getCustomerId());
+		return duantukeLikeMapper.selectByExample(example);
+	}
+	
+
+}
