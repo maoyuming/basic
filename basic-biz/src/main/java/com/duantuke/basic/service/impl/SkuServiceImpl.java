da0ce986fda3740cac7f4e8e8d7259290fe4ac42
姜贺 <he.jiang@fangbaba.com>
2016-06-14 10:35:26 +0800

    解决报错

--- basic-biz/src/main/java/com/duantuke/basic/service/impl/SkuServiceImpl.java
+++ basic-biz/src/main/java/com/duantuke/basic/service/impl/SkuServiceImpl.java
@@ -109,7 +109,7 @@ public class SkuServiceImpl implements SkuService {
 				
 				
 				if(MapUtils.isNotEmpty(priceMap)){
-					roomTypeInfo.setPrices(priceMap.get(roomType.getRoomtypeId()));
+					roomTypeInfo.setPrices(priceMap.get(roomType.getSkuId()));
 					if(MapUtils.isNotEmpty(roomTypeInfo.getPrices())){
 						for (Entry<String, BigDecimal> entry : roomTypeInfo.getPrices().entrySet()) {
 							if(entry.getValue()!=null){
