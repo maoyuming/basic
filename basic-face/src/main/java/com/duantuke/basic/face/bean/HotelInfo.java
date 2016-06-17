07d54dce3c503a0b8c66f78c8c07b3389e6d628c
maoyuming <mao_yuming@126.com>
2016-06-17 09:40:46 +0800

    修改hotelinfo

--- basic-face/src/main/java/com/duantuke/basic/face/bean/HotelInfo.java
+++ basic-face/src/main/java/com/duantuke/basic/face/bean/HotelInfo.java
@@ -4,19 +4,18 @@ import java.io.Serializable;
 import java.util.List;
 
 import com.duantuke.basic.po.Hotel;
-import com.duantuke.basic.po.RoomType;
 
 public class HotelInfo extends Hotel implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
-	private List<RoomType> roomTypes;
+	private List<RoomTypeInfo> roomTypes;
 
-	public List<RoomType> getRoomTypes() {
+	public List<RoomTypeInfo> getRoomTypes() {
 		return roomTypes;
 	}
 
-	public void setRoomTypes(List<RoomType> roomTypes) {
+	public void setRoomTypes(List<RoomTypeInfo> roomTypes) {
 		this.roomTypes = roomTypes;
 	}
 }
