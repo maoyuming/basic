957a3b55f6258d9b8df9665080b5e1725f901d55
tankai <kai.tank@imike.com>
2016-06-22 16:46:18 +0800

    增加销售接口

--- basic-biz/src/main/java/com/duantuke/basic/service/impl/SaleServiceImpl.java
+++ basic-biz/src/main/java/com/duantuke/basic/service/impl/SaleServiceImpl.java
@@ -9,12 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.duantuke.basic.enums.IsvisibleEnum;
+import com.duantuke.basic.exception.OpenException;
 import com.duantuke.basic.face.service.SaleService;
 import com.duantuke.basic.mappers.SaleMapper;
-import com.duantuke.basic.po.Boss;
-import com.duantuke.basic.po.BossExample;
+import com.duantuke.basic.mappers.SalesRHotelMapper;
 import com.duantuke.basic.po.Sale;
 import com.duantuke.basic.po.SaleExample;
+import com.duantuke.basic.po.SalesRHotel;
+import com.duantuke.basic.po.SalesRHotelExample;
 
 /**
  * c用户信息相关接口
@@ -27,7 +29,12 @@ public class SaleServiceImpl implements SaleService {
 	
 	@Autowired
 	private SaleMapper saleMapper;
+	@Autowired
+	private SalesRHotelMapper salesRHotelMapper;
 
+	/**
+	 * 根据手机号码查询销售消息
+	 */
 	@Override
 	public Sale querySaleByPhone(String phone) {
 		
@@ -40,6 +47,63 @@ public class SaleServiceImpl implements SaleService {
 			return models.get(0);
 		}
 		return null;
+	}
+	
+	/**
+	 * 根据销售id查询销售信息
+	 * @param saleId
+	 * @return
+	 */
+	public Sale querySaleById(Long saleId) {
+		
+		SaleExample example = new SaleExample();
+		SaleExample.Criteria hoCriteria = example.createCriteria();
+		hoCriteria.andSalesIdEqualTo(saleId);
+		hoCriteria.andIsvisibleEqualTo(IsvisibleEnum.yes.getCode());
+		List<Sale> models =  saleMapper.selectByExample( example);
+		if(CollectionUtils.isNotEmpty(models)){
+			return models.get(0);
+		}
+		return null;
+	}
+
+	@Override
+	public Sale querySaleByHotelId(Long hotelId) {
+		//根据酒店id查询销售id
+		List<SalesRHotel> list=	querySalesRHotel(hotelId, null);
+		if(CollectionUtils.isNotEmpty(list)){
+			for (SalesRHotel salesRHotel : list) {
+				Sale sale = querySaleById(salesRHotel.getSaleId());
+				return sale;
+			}
+		}
+		return null;
+	}
+	
+	
+	/**
+	 * 根据酒店id或者销售id查询关系
+	 * @param hotelId
+	 * @param saleId
+	 * @return
+	 */
+	public List<SalesRHotel> querySalesRHotel(Long hotelId,Long saleId){
+
+		
+		if(hotelId==null&&saleId==null){
+			throw new OpenException("酒店id和销售id全是空");
+		}
+		
+		SalesRHotelExample example = new SalesRHotelExample();
+		SalesRHotelExample.Criteria hoCriteria = example.createCriteria();
+		if(hotelId!=null){
+			hoCriteria.andHotelIdEqualTo(hotelId);
+		}
+		if(saleId!=null){
+			hoCriteria.andSaleIdEqualTo(saleId);
+		}
+		List<SalesRHotel> models =  salesRHotelMapper.selectByExample( example);
+		return models;
 	}
 
 
--- basic-face/src/main/java/com/duantuke/basic/face/service/SaleService.java
+++ basic-face/src/main/java/com/duantuke/basic/face/service/SaleService.java
@@ -1,9 +1,39 @@
 package com.duantuke.basic.face.service;
 
+import java.util.List;
+
 import com.duantuke.basic.po.Sale;
+import com.duantuke.basic.po.SalesRHotel;
 
 public interface SaleService {
 	
+
+	/**
+	 * 根据手机号码查询销售消息
+	 */
 	public Sale querySaleByPhone(String phone);
+	
+	/**
+	 * 根据销售id查询销售信息
+	 * @param saleId
+	 * @return
+	 */
+	public Sale querySaleById(Long saleId) ;
+
+	/**
+	 * 根据酒店id查询销售信息
+	 * @param hotelId
+	 * @return
+	 */
+	public Sale querySaleByHotelId(Long hotelId) ;
+	
+	
+	/**
+	 * 根据酒店id或者销售id查询关系
+	 * @param hotelId
+	 * @param saleId
+	 * @return
+	 */
+	public List<SalesRHotel> querySalesRHotel(Long hotelId,Long saleId);
 	
 }
