dd99f2be2b2764145d29aef131bb1cac77656632
姜贺 <he.jiang@fangbaba.com>
2016-06-22 12:13:36 +0800

    获取IP地址测试接口

--- basic-web/src/main/java/com/duantuke/basic/controller/UtilController.java
+++ basic-web/src/main/java/com/duantuke/basic/controller/UtilController.java
@@ -1,9 +1,13 @@
 package com.duantuke.basic.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -346,5 +350,40 @@ public class UtilController extends BaseController {
 		}else{
 			return false;
 		}
+	}
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public ResponseEntity<String> test(HttpServletRequest request) {
+		     String ipAddress = null;   
+		     //ipAddress = this.getRequest().getRemoteAddr();   
+		     ipAddress = request.getHeader("x-forwarded-for");   
+		     if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {   
+		      ipAddress = request.getHeader("Proxy-Client-IP");   
+		     }   
+		     if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {   
+		         ipAddress = request.getHeader("WL-Proxy-Client-IP");   
+		     }   
+		     if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {   
+		      ipAddress = request.getRemoteAddr();   
+		      if(ipAddress.equals("127.0.0.1")){   
+		       //根据网卡取本机配置的IP   
+		       InetAddress inet=null;   
+		    try {   
+		     inet = InetAddress.getLocalHost();   
+		    } catch (UnknownHostException e) {   
+		     e.printStackTrace();   
+		    }   
+		    ipAddress= inet.getHostAddress();   
+		      }   
+		            
+		     }   
+		  
+		     //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割   
+		     if(ipAddress!=null && ipAddress.length()>15){ //"***.***.***.***".length() = 15   
+		         if(ipAddress.indexOf(",")>0){   
+		             ipAddress = ipAddress.substring(0,ipAddress.indexOf(","));   
+		         }   
+		     }   
+		return new ResponseEntity<String>(ipAddress, HttpStatus.OK);
 	}
 }
