af47a07caefa971985ee7f86bee04e3d66d75a5f
姜贺 <he.jiang@fangbaba.com>
2016-07-06 17:22:36 +0800

    加快es插入速度

--- basic-biz/src/main/java/com/duantuke/basic/service/impl/HotelSearchServiceImpl.java
+++ basic-biz/src/main/java/com/duantuke/basic/service/impl/HotelSearchServiceImpl.java
@@ -29,6 +29,7 @@ import com.duantuke.basic.face.esbean.query.TeamSkuQueryBean;
 import com.duantuke.basic.face.service.HotelSearchService;
 import com.duantuke.basic.face.service.PriceService;
 import com.duantuke.basic.face.service.TagService;
+import com.duantuke.basic.po.Meal;
 import com.duantuke.basic.po.Tag;
 import com.duantuke.basic.service.IHotelService;
 import com.duantuke.basic.service.IMealService;
@@ -94,8 +95,57 @@ public class HotelSearchServiceImpl implements HotelSearchService {
 	public void initEs(Long hotelId) {
 		logger.info("HotelSearchServiceImpl initEs begin:{}", hotelId);
 		List<HotelInputBean> esInputlist = ihotelService.queryEsInputHotels(hotelId);
+		List<Tag> tags = tagService.queryTagsByHotelId(hotelId);
+		List<MealInputBean> meals = imealService.queryEsInputMealsByHotelid(hotelId);
+		List<TeamSkuInputBean> teamskus = iteamSkuService.queryEsInputTeamSkusByHotelId(hotelId);
 		
-		//final CountDownLatch doneSingal = new CountDownLatch(esInputlist.size());
+		Map<Long, List<Tag>> tagsmap = new HashMap<Long, List<Tag>>();
+		Map<Long, List<MealInputBean>> mealsmap = new HashMap<Long, List<MealInputBean>>();
+		Map<Long, List<TeamSkuInputBean>> teamskusmap = new HashMap<Long, List<TeamSkuInputBean>>();
+		
+		for (Tag tag:tags) {
+			Long supplierId = tag.getHotelId();
+			if(supplierId==null)
+				continue;
+			List<Tag> list= tagsmap.get(supplierId);
+			if(CollectionUtils.isNotEmpty(list)){
+				list.add(tag);
+			}else{
+				list = new ArrayList<Tag>();
+				list.add(tag);
+			}
+			tagsmap.put(supplierId, list);
+		}
+		for (MealInputBean meal:meals) {
+			String supplierIdStr = meal.getSupplierId();
+			if(supplierIdStr==null)
+				continue;
+			Long supplierId = Long.parseLong(supplierIdStr);
+			List<MealInputBean> list= mealsmap.get(supplierId);
+			if(CollectionUtils.isNotEmpty(list)){
+				list.add(meal);
+			}else{
+				list = new ArrayList<MealInputBean>();
+				list.add(meal);
+			}
+			mealsmap.put(supplierId, list);
+		}
+		for (TeamSkuInputBean teamsku:teamskus) {
+			String supplierIdStr = teamsku.getSupplierId();
+			if(supplierIdStr==null)
+				continue;
+			Long supplierId = Long.parseLong(supplierIdStr);
+			List<TeamSkuInputBean> list= teamskusmap.get(supplierId);
+			if(CollectionUtils.isNotEmpty(list)){
+				list.add(teamsku);
+			}else{
+				list = new ArrayList<TeamSkuInputBean>();
+				list.add(teamsku);
+			}
+			teamskusmap.put(supplierId, list);
+		}
+		
+		final CountDownLatch doneSingal = new CountDownLatch(esInputlist.size());
 		//多线程
 		for (final HotelInputBean hotelInputBean:esInputlist) {
 			ThreadPoolUtil.pool.execute(new Runnable() {
@@ -107,36 +157,40 @@ public class HotelSearchServiceImpl implements HotelSearchService {
 						}
 						hotelInputBean.setCreatetime(DateUtil.dateToStr(DateUtil.getNowDate(), "yyyy-MM-dd HH:mm"));
 						//保存tag
-						List<Tag> taglist = tagService.queryTagsByHotelId(hotelInputBean.getHotelId());
-						for (Tag tag:taglist) {
-							Map<String,String> map = dozerMapper.map(tag, Map.class);
-							if(tag.getTagGroupId().equals(1l)){
-								hotelInputBean.getTaggroup_1().add(map);
-							}else if(tag.getTagGroupId().equals(2l)){
-								hotelInputBean.getTaggroup_2().add(map);
-							}else if(tag.getTagGroupId().equals(3l)){
-								hotelInputBean.getTaggroup_3().add(map);
-							}else if(tag.getTagGroupId().equals(4l)){
-								hotelInputBean.getTaggroup_4().add(map);
+						//List<Tag> taglist = tagService.queryTagsByHotelId(hotelInputBean.getHotelId());
+						List<Tag> taglist = tagsmap.get(hotelInputBean.getHotelId());
+						if(CollectionUtils.isNotEmpty(taglist)){
+							for (Tag tag:taglist) {
+								Map<String,String> map = dozerMapper.map(tag, Map.class);
+								if(tag.getTagGroupId().equals(1l)){
+									hotelInputBean.getTaggroup_1().add(map);
+								}else if(tag.getTagGroupId().equals(2l)){
+									hotelInputBean.getTaggroup_2().add(map);
+								}else if(tag.getTagGroupId().equals(3l)){
+									hotelInputBean.getTaggroup_3().add(map);
+								}else if(tag.getTagGroupId().equals(4l)){
+									hotelInputBean.getTaggroup_4().add(map);
+								}
 							}
 						}
 						hotelInputBean.setPrices(getPrices(hotelInputBean.getHotelId()));
-						List<MealInputBean> meals = imealService.queryEsInputMealsByHotelid(hotelInputBean.getHotelId());
+						//List<MealInputBean> meals = imealService.queryEsInputMealsByHotelid(hotelInputBean.getHotelId());
+						List<MealInputBean> meals = mealsmap.get(hotelInputBean.getHotelId());
 						hotelInputBean.setMeals(meals);
 						
-						List<TeamSkuInputBean> teamskus = iteamSkuService.queryEsInputTeamSkusByHotelId(hotelInputBean.getHotelId());
+						//List<TeamSkuInputBean> teamskus = iteamSkuService.queryEsInputTeamSkusByHotelId(hotelInputBean.getHotelId());
+						List<TeamSkuInputBean> teamskus = teamskusmap.get(hotelInputBean.getHotelId());
 						hotelInputBean.setTeamskus(teamskus);
-						esutil.signleAddDocument(hotelInputBean, hotelInputBean.getHotelId()+"");
 					}catch (Exception e) {
 						logger.error("HotelSearchServiceImpl initEs error", e);
-				    } //finally {
-					   //doneSingal.countDown();
-					  // ThreadPoolUtil.threadSleep(ThreadPoolUtil.threadSleep);
-				    //}
+				    } finally {
+					   doneSingal.countDown();
+					   ThreadPoolUtil.threadSleep(ThreadPoolUtil.threadSleep);
+				    }
 				}
 			});
 		}
-		/*try {
+		try {
 			doneSingal.await();
 		} catch (InterruptedException e) {
 			logger.error("HotelSearchServiceImpl initEs InterruptedException",e);
@@ -145,7 +199,7 @@ public class HotelSearchServiceImpl implements HotelSearchService {
 			esutil.batchAddDocument(esInputlist);
 		}else{
 			logger.warn("HotelSearchServiceImpl initEs warn list is empty:{}", hotelId);
-		}*/
+		}
 		logger.info("HotelSearchServiceImpl initEs end:{}", hotelId);
 	}
 	
